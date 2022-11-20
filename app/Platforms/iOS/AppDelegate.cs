dd6806fc8066b9014e8a713b1edcb5dcb06c5e88
Gerben van de Wiel <g.van.de.wiel@betabit.nl>
2022-11-20 11:43:31 +0100

    Basic app setup #3

---
+++ app/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Foodwaste;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => FoodwasteProgram.CreateMauiApp();
+}
