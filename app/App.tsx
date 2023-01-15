18a9423f38316fca312a4d7b24fd130bbaf36b88
jeremoose <j.4.bergen@gmail.com>
2023-01-15 14:45:48 +0100

    wip

--- app/App.tsx
+++ app/App.tsx
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  SectionList,
+  StatusBar,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class Product {
+  id: number;
+  name: string;
+  expiryDate: Date;
+
+  constructor(id: number, name: string, expiryDate: Date = new Date()) {
+    this.id = id;
+    this.name = name;
+    this.expiryDate = expiryDate;
+  }
 }
+
+const NewProduct = (id: number, name: string, expiryDate: Date = new Date()) => {
+  return new Product(id, name, expiryDate);
+};
+
+const DATA = [
+  {
+    title: "Pantry",
+    data: [
+      NewProduct(1, "Black Beans", new Date(Date.now() + 259200000)),
+      NewProduct(2, "Cookies"),
+      NewProduct(3, "Rice"),
+    ],
+  },
+  {
+    title: "Fridge",
+    data: [
+      NewProduct(4, "Chicken"),
+      NewProduct(5, "Cucumber"),
+      NewProduct(6, "Milk"),
+      NewProduct(6, "Paprika"),
+    ],
+  },
+];
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <SectionList
+      sections={DATA}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <View style={styles.item}>
+          <Text style={styles.title}>Product: {item.name}</Text>
+          <Text style={styles.title}>
+            Exp: {item.expiryDate.getDate()}-{item.expiryDate.getMonth() + 1}-{item.expiryDate.getFullYear()}
+          </Text>
+        </View>
+      )}
+      renderSectionHeader={({ section: { title } }) => (
+        <Text style={styles.header}>{title}</Text>
+      )}
+    />
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
   },
 });
+
+export default App;
